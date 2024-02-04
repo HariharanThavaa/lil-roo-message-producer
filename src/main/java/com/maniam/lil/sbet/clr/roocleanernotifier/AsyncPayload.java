0771e3cf57d385fe52ff293304475b53b477fd4f
Hariharan <harimaniam@gmail.com>
2024-02-04 19:11:05 +0000

    HH-05-05:Building Spring Message Producers.

---
+++ src/main/java/com/maniam/lil/sbet/clr/roocleanernotifier/AsyncPayload.java
@@ -0,0 +1,21 @@
+package com.maniam.lil.sbet.clr.roocleanernotifier;
+public class AsyncPayload {
+    private long id;
+    private String model;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
---
+++ src/test/java/com/maniam/lil/sbet/clr/roocleanernotifier/RooCleanerNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maniam.lil.sbet.clr.roocleanernotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RooCleanerNotifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
